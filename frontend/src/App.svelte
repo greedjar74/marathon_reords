bc8280280b4f9af4bf149283e4b7e1968710e600
김홍석 <kimhongseok@gimhongseog-ui-MacBookPro.local>
2024-04-17 01:21:34 +0900

    조건 부여 테스트중

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,12 +2,10 @@
   import Router from 'svelte-spa-router'
   import Home from "./routes/Home.svelte"
   import Detail from './routes/Detail.svelte';
-  import Update from './routes/Update.svelte';
 
   const routes = {
     '/': Home, // '/'과 '/records'가 맵핑
     '/record_detail/:record_id': Detail,
-    '/update_record': Update
   }
 </script>
 
--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -2,6 +2,8 @@
     let record_list = []
     import { link } from 'svelte-spa-router'
 
+    let endpoint = "records"; // Default endpoint
+
     function get_record_list() {
         fetch("http://127.0.0.1:8000/records").then((response) => {
             response.json().then((json) => {
@@ -11,7 +13,19 @@
     }
 
     get_record_list()
+
+    function switchEndpoint(newEndpoint) {
+      endpoint = newEndpoint;
+      get_record_list().then(data => {
+        record_list = data;
+      });
+    }
 </script>
+
+<select bind:value={endpoint} on:change={() => switchEndpoint(endpoint)}>
+    <option value="records">Records</option>
+    <option value="upate_record">Update Record</option>
+  </select>
 
 <h1><a use:link href="/update_record">새로운 기록이 있다면 입력해주세요</a></h1>
 <h3>
